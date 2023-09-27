afb5a876361ae14adc693c7e1268fbf42ad4430c
fgomez-d <fgomez-d@student.42malaga.com>
2023-09-27 16:32:07 +0200

    Add initial data structure to headerfile

--- cub3d.h
+++ cub3d.h
@@ -8,4 +8,44 @@
 # include <string.h>
 # include <unistd.h>
 
+# define PI 3.14159265359
+
+typedef struct s_color
+{
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+	uint8_t	a;
+}		t_color;
+
+typedef struct s_map
+{
+	s_player	*player;
+	int			**cells;
+	int			max_x;
+	int			max_y;
+	char		*no_texture;
+	char		*so_texture;
+	char		*we_texture;
+	char		*ea_texture;
+	s_color		*ceiling;
+	s_color		*floor;
+}		t_map;
+
+typedef struct s_player
+{
+	int		start_x;
+	int		start_y;
+	float	x;
+	float	y;
+	float	angle;
+}		t_player;
+
+enum e_map_cell
+{
+	empty,
+	wall,
+	outside,
+};
+
 #endif
